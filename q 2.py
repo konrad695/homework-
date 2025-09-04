741e596d8d008415df35b53f68486f2008370f3c
konrad695 <21krutkowski.acc@lwetb.ie>
2025-09-04 09:31:20 +0100

    Add files via upload

---
+++ q 2.py
@@ -0,0 +1,13 @@
+'''
+Author: konrad
+Date: 3 September 2025
+Description: If, else, elif excercise
+
+'''
+
+num = input('enter a number less then 20')
+num = float(num)
+if(num<20):
+    print('thanks')
+else:
+     print('error')
