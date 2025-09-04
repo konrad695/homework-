75efad64cbfc4865b365eedfc2b060cc05d00bd1
konrad695 <21krutkowski.acc@lwetb.ie>
2025-09-04 09:27:32 +0100

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,6 +1,12 @@
-user_name = input(' Enter your name ')
-print('hello',user_name)
- 
-user_surname = input(' Whats your surname? ')
-print('hello', user_name, user_surname)
- 
+'''
+Author: konrad
+Date: 3 September 2025
+Description: If, else, elif excercise
+
+'''
+num1 = input('write a number')
+num2 = input('write another number')
+if(num1>num2):
+    print('your numbers are: ', num2, 'and' , num1)
+elif(num1<num2):
+    print('your numbers are: ', num1, 'and' , num2) 
