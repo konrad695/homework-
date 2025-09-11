1c7cd7138a391d7afa6d52e00347ed87d8795464
konrad695 <21krutkowski.acc@lwetb.ie>
2025-09-11 09:39:17 +0100

    Add files via upload

---
+++ loops q5.py
@@ -0,0 +1,10 @@
+'''
+Author: konrad
+Date: 11 September 2025
+Description: for loops
+'''
+
+number=input('enter a number')
+number = int(number)
+for i in range (1,13):
+    print(number,'x',i,'=',number*i)
