b37176a8fa03648772ed02305046a5eae0956de4
konrad695 <21krutkowski.acc@lwetb.ie>
2025-09-11 09:23:47 +0100

    Add files via upload

---
+++ loops q4.py
@@ -0,0 +1,14 @@
+'''
+Author: konrad
+Date: 11 September 2025
+Description: for loops
+'''
+total = 0
+num = input('enter sa number')
+num = int(num)
+for i in range (num):
+    if i%2 !=0:
+        total = total+i
+print(total)
+        
+ 
